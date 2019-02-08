38efa8ff09300a0d587d3164d1b3810762b3bc68
Ramon Huidobro <hola@ramonhuidobro.com>
2019-02-08 15:04:54 +0100

    Use text select

--- src/components/ThingSelect.js
+++ src/components/ThingSelect.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
+import Select from "react-select";
 
 const GET_THING_NAMES = gql`
 {
@@ -12,20 +13,26 @@ const GET_THING_NAMES = gql`
 `;
 
 export default class ThingSelect extends Component {
+
+  thingOptions (things) {
+  }
+
   render () {
     return <Query query={GET_THING_NAMES}>
       {({ loading, error, data }) => {
         if (loading) return "Loading...";
         if (error) return `Error! ${error.message}`;
 
+        let options = data.things.map(thing => {
+          return {
+            value: thing.id,
+            label: thing.name,
+          }
+        });
+
         return (
-          <select name="thing" >
-            {data.things.map(dog => (
-              <option key={dog.id} value={dog.breed}>
-                {dog.breed}
-              </option>
-            ))}
-          </select>
+          <Select options={options}>
+          </Select>
         );
       }}
     </Query>
