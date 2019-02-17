4999aa0bacbdd18693d1811730c4ae3158ecb7f2
Ramon Huidobro <hola@ramonhuidobro.com>
2019-02-17 20:29:03 +0100

    Add rating capabilities to prop kind

--- src/components/ThingLogger/RatingProp.tsx
+++ src/components/ThingLogger/RatingProp.tsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import Rating from 'react-rating';
 import Prop from '../../interfaces/Prop';
 import PropInput from '../../interfaces/PropInput';
 
-export default class RatingProp extends Component<PropInput> {
+export default class RatingProp extends Component<PropInput, { rating : number }> {
+  constructor(props : PropInput) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentWillMount() {
+    this.setState({
+      rating: 3
+    });
+  }
+
+  handleChange(rating : number) {
+    this.setState({
+      rating: rating
+    });
+  }
+
   render() {
-    return <h1>I AM ALSO ALIVE</h1>;
+    return <Rating
+    initialRating={this.state.rating}
+    onChange={this.handleChange}
+      />;
   }
 }
