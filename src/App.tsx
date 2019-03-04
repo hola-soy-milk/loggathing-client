7792e7d1656272ce069694599ff20d919dd11e55
Ramon Huidobro <hola@ramonhuidobro.com>
2019-03-04 21:47:28 +0100

    Add the TITLE

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import logo from './logo.svg';
 import { ApolloProvider } from "react-apollo";
 import api from "./core/Api";
 import ThingLogger from "./components/ThingLogger";
-import './App.css';
 import 'bulma/css/bulma.min.css';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <section className="section">
         <div className="container">
-          <div className="App">
-          <ApolloProvider client={api}>
-          <ThingLogger />
-          </ApolloProvider>
+          <div className="content">
+            <h1>Go ahead and <em>Loggathing</em>!</h1>
+            <div className="App">
+            <ApolloProvider client={api}>
+            <ThingLogger />
+            </ApolloProvider>
+            </div>
           </div>
         </div>
       </section>
